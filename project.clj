b2435868a29c524bc5a7b031e14a908c03b28345
Paul Sabda <sabdaps@gmail.com>
2014-12-27 01:46:21 +0700

    update 0.1.11

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject zenedu/zenpack "0.1.10"
+(defproject zenedu/zenpack "0.1.11"
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [lib-noir "0.9.5"]
                  [http-kit "2.1.19"]
@@ -9,9 +9,16 @@
   :url "https://github.com/zeniuseducation/zenpack"
   :plugins [[codox "0.8.10"]
             [lein-autoexpect "1.4.2"]]
+  :scm {:name "git"
+        :url "https://github.com/zeniuseducation/zenpack"}
+  :signing {:gpg-key "3F84379F"}
   :description "Starter pack for creating basic clojure web app"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :pom-addition [:developers [:developer
+                              [:name "Zenius Education"]
+                              [:url "http://github.com/zeniuseducation"]
+                              [:timezone "+7"]]]
   :repositories [["releases" {:url "http://clojars.org/repo"
                               :creds :gpg}]])
 
